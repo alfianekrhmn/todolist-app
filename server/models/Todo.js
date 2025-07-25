73cb608c0eae70e9be93bb52807e023d1389891a
alfianekrhmn <alfian.eka481@gmail.com>
2025-07-25 13:07:32 +0700

    feat: add Todo model with title and completed status

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -15,6 +15,9 @@ const TodoSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+},
+{
+    timestamps: true
 });
 
 const Todo = mongoose.model('Todo', TodoSchema);
